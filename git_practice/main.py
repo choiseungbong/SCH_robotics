685cc4f10aa997b5706c07aa68499db5dc5d2e29
choiseungbong/SCH_robotics <als001116@naver.com>
2023-11-29 14:20:57 +0900

    make main to use dict ops

--- git_practice/main.py
+++ git_practice/main.py
@@ -1,9 +1,20 @@
+# main.py
 import list_ops as lo
+import dict_ops as do
 
 if __name__ == "__main__":
     foo = [1,2,3]
     bar = [4,5,6]
+    print(f"foo: {foo}, bar: {bar}")
     print("foo+bar=", lo.add(foo, bar))
     print("foo-bar=", lo.subtract(foo, bar))
     print("foo*bar=", lo.multiply(foo, bar))
     print("foo/bar=", lo.divide(foo, bar))
+    
+    foo = {"Java": 79, "Cpp": 45, "Python": 99}
+    bar = {"Java": 36, "Python": 56, "Ruby": 63}
+    print(f"foo: {foo}, bar: {bar}")
+    print("foo+bar=", do.add(foo, bar))
+    print("foo-bar=", do.subtract(foo, bar))
+    print("foo*bar=", do.multiply(foo, bar))
+    print("foo/bar=", do.divide(foo, bar))
