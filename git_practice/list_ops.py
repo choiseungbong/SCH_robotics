6de2a5721a3cb83b5fd6ba352dba672c84a4a4e1
choiseungbong/SCH_robotics <als001116@naver.com>
2023-11-29 14:14:41 +0900

    implement multiply and substract

--- git_practice/list_ops.py
+++ git_practice/list_ops.py
@@ -9,3 +9,15 @@ def subtract(foo, bar):
     for f, b in zip(foo, bar):
         out.append(f - b)
     return out
+    
+    def multiply(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f * b)
+    return out
+
+def divide(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f / b)
+    return out
