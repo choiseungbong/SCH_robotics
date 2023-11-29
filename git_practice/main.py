928f13fed47e3031c193a6f94ac8d045bd299e43
choiseungbong/SCH_robotics <als001116@naver.com>
2023-11-29 14:15:20 +0900

    change main to use list ops

--- git_practice/main.py
+++ git_practice/main.py
@@ -1,3 +1,9 @@
+import list_ops as lo
+
 if __name__ == "__main__":
-    print("hello python, hi ros.")
-    print("Python is the best language for ros")
+    foo = [1,2,3]
+    bar = [4,5,6]
+    print("foo+bar=", lo.add(foo, bar))
+    print("foo-bar=", lo.subtract(foo, bar))
+    print("foo*bar=", lo.multiply(foo, bar))
+    print("foo/bar=", lo.divide(foo, bar))
